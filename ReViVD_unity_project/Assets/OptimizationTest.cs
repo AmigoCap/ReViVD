a99a72706251a2ea17b9492ab2513b3e82d8a2bc
yohhaan <yohan.beugin@ecl17.ec-lyon.fr>
2018-12-11 14:36:13 +0100

    Unity project added

---
+++ ReViVD_unity_project/Assets/OptimizationTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptimizationTest : MonoBehaviour {
+    
+    class Test {
+        public Test() { Debug.Log("beep"); }
+        public int a = 0;
+    }
+
+    List<Test> L;
+
+    IReadOnlyList<Test> getList() {
+        return L;
+    }
+
+	// Use this for initialization
+	void Start () {
+        L = new List<Test>();
+        L.Add(new Test());
+        getList()[0].a = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
