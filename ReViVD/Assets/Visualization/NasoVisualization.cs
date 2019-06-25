76aba94c6ae2c92bbc067ed53a9997add248933b
FHomps <francois.homps@ecl17.ec-lyon.fr>
2019-06-25 16:25:13 +0200

    Added extra data to NasoAtoms and reworked their file structure and loading scheme

--- ReViVD/Assets/Visualization/NasoVisualization.cs
+++ ReViVD/Assets/Visualization/NasoVisualization.cs
@@ -31,9 +31,6 @@ namespace Revivd {
         public Vector3 minPoint = new Vector3(-500, -500, -500);
 
         protected override bool LoadFromFile() {
-            if (filenameBase == "")
-                return false;
-
             int[] keptParticles = new int[n_particles];
             Color32[] pathColors = new Color32[n_particles];
 
@@ -58,60 +55,108 @@ namespace Revivd {
             for (int i = 0; i < n_particles; i++) {
                 GameObject go = new GameObject(keptParticles[i].ToString());
                 go.transform.parent = transform;
-                paths.Add(go.AddComponent<NasoPath>());
+                NasoPath p = go.AddComponent<NasoPath>();
+                p.atoms = new List<NasoAtom>(n_instants);
+                paths.Add(p);
                 pathColors[i] = Random.ColorHSV();
             }
 
             Tools.AddClockStop("created paths");
 
-            int currentFileNumber = instantsStart / 50 + 1;
-            int instant = instantsStart;
-            int localInstant = instantsStart % 50;
-            BinaryReader br = new BinaryReader(File.Open(filenameBase + currentFileNumber.ToString("0000") + ".bytes", FileMode.Open));
-            Tools.AddClockStop("Loaded file " + currentFileNumber.ToString("0000"));
-            for (int t = 0; t < n_instants; t++) {
-                if (localInstant >=  50) {
-                    Tools.AddClockStop("Finished loading from file " + currentFileNumber.ToString("0000"));
+            BinaryReader br;
 
-                    localInstant %= 50;
-                    br.Dispose();
-                    Tools.AddClockStop("Closed file " + currentFileNumber.ToString("0000"));
+            int bundleSize = 50;
 
-                    currentFileNumber++;
-                    br = new BinaryReader(File.Open(filenameBase + currentFileNumber.ToString("0000") + ".bytes", FileMode.Open));
-                    Tools.AddSubClockStop("BinaryReader creation");
+            int lastKeptInstant = instantsStart + instantsStep * (n_instants - 1);
+            for (int bundle = 0; bundle < 50 && bundle * bundleSize < lastKeptInstant; bundle++) {
+                if ((bundle + 1) * bundleSize <= instantsStart)
+                    continue;
 
-                    Tools.AddClockStop("Loaded file " + currentFileNumber.ToString("0000"));
-                }
+                int currentFileNumber = bundle + 1;
+                br = new BinaryReader(File.Open(filenameBase + currentFileNumber.ToString("0000"), FileMode.Open));
 
                 for (int i = 0; i < n_particles; i++) {
-                    Vector3 point = new Vector3();
-                    br.BaseStream.Position = (3 * 1000000 * localInstant + keptParticles[i]) * 8;
-                    point.x = ((float)br.ReadDouble() - 317) * sizeCoeff;
-                    br.BaseStream.Position += 1000000 * 8 - 8; //Going back 8 bytes because reading the data advances the position
-                    point.z = ((float)br.ReadDouble() - 317) * sizeCoeff;
-                    br.BaseStream.Position += 1000000 * 8 - 8;
-                    point.y = ((float)br.ReadDouble() - 317) * sizeCoeff;
+                    for (int local_t = 0; local_t < bundleSize; local_t++) {
+                        int global_t = bundle * bundleSize + local_t;
+                        if (global_t < instantsStart || (global_t - instantsStart) % instantsStep != 0)
+                            continue;
 
-                    point = Vector3.Max(point, minPoint);
-                    point = Vector3.Min(point, maxPoint);
+                        br.BaseStream.Position = (bundleSize * keptParticles[i] + local_t) * 6 * 4;
+                        Vector3 point = new Vector3 {
+                            x = br.ReadSingle() - 317f,
+                            z = br.ReadSingle() - 317f,
+                            y = br.ReadSingle() - 317f
+                        };
+                        point *= sizeCoeff;
 
-                    NasoAtom a = new NasoAtom() {
-                        time = (float)instant,
-                        point = point,
-                        path = paths[i],
-                        indexInPath = t
-                    };
+                        point = Vector3.Max(point, minPoint);
+                        point = Vector3.Min(point, maxPoint);
 
-                    a.BaseColor = pathColors[i];
-                    paths[i].atoms.Add(a);
+                        NasoAtom a = new NasoAtom() {
+                            time = (float)global_t,
+                            point = point,
+                            path = paths[i],
+                            indexInPath = (global_t - instantsStart) / instantsStep,
+                            speed = br.ReadSingle(),
+                            accel = br.ReadSingle(),
+                            power = br.ReadSingle()
+                        };
+
+                        //a.BaseColor = Color32.Lerp(Color.blue, Color.red, (a.speed - 0.08f) / (0.39f - 0.08f));
+                        a.BaseColor = Color32.Lerp(Color.blue, Color.red, (a.power - 0.006f) / (0.148f - 0.006f));
+                        if (a.power < 0)
+                            a.BaseColor = Color.green;
+                        paths[i].atoms.Add(a);
+                    }
                 }
+                br.Dispose();
 
-                Tools.AddSubClockStop("Loaded particles for instant " + instant);
-
-                instant += instantsStep;
-                localInstant += instantsStep;
+                Tools.AddClockStop("Loaded bundle " + currentFileNumber.ToString("0000"));
             }
+
+            //for (int t = 0; t < n_instants; t++) {
+            //    if (localInstant >=  50) {
+            //        Tools.AddClockStop("Finished loading from file " + currentFileNumber.ToString("0000"));
+
+            //        localInstant %= 50;
+            //        br.Dispose();
+            //        Tools.AddClockStop("Closed file " + currentFileNumber.ToString("0000"));
+
+            //        currentFileNumber++;
+            //        br = new BinaryReader(File.Open(filenameBase + currentFileNumber.ToString("0000") + ".bytes", FileMode.Open));
+            //        Tools.AddSubClockStop("BinaryReader creation");
+
+            //        Tools.AddClockStop("Loaded file " + currentFileNumber.ToString("0000"));
+            //    }
+
+            //    for (int i = 0; i < n_particles; i++) {
+            //        Vector3 point = new Vector3();
+            //        br.BaseStream.Position = (6 * 1000000 * localInstant + 6 * keptParticles[i]) * 4;
+            //        point.x = ((float)br.ReadDouble() - 317) * sizeCoeff;
+            //        br.BaseStream.Position += 1000000 * 8 - 8; //Going back 8 bytes because reading the data advances the position
+            //        point.z = ((float)br.ReadDouble() - 317) * sizeCoeff;
+            //        br.BaseStream.Position += 1000000 * 8 - 8;
+            //        point.y = ((float)br.ReadDouble() - 317) * sizeCoeff;
+
+            //        point = Vector3.Max(point, minPoint);
+            //        point = Vector3.Min(point, maxPoint);
+
+            //        NasoAtom a = new NasoAtom() {
+            //            time = (float)instant,
+            //            point = point,
+            //            path = paths[i],
+            //            indexInPath = t
+            //        };
+
+            //        a.BaseColor = pathColors[i];
+            //        paths[i].atoms.Add(a);
+            //    }
+
+            //    Tools.AddSubClockStop("Loaded particles for instant " + instant);
+
+            //    instant += instantsStep;
+            //    localInstant += instantsStep;
+            //}
 
             Tools.EndClock();
 
@@ -129,6 +174,9 @@ namespace Revivd {
         }
 
         public class NasoAtom : TimeAtom {
+            public float speed;
+            public float accel;
+            public float power;
         }
     }
 
