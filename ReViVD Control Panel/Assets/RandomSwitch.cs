1b272c012a78dafa089587d965db3701e8aa832a
FHomps <francois.homps@ecl17.ec-lyon.fr>
2019-07-05 16:04:33 +0200

    Added sampling section

---
+++ ReViVD Control Panel/Assets/RandomSwitch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomSwitch : MonoBehaviour
+{
+    public Toggle toggle;
+    public GameObject randomLine;
+    public GameObject standardLine;
+    public RectTransform rangeLineTransform;
+
+    float xPosRandom;
+    float xPosStandard;
+
+    void Switch() {
+        randomLine.SetActive(toggle.isOn);
+        standardLine.SetActive(!toggle.isOn);
+        rangeLineTransform.localPosition = new Vector3(toggle.isOn ? xPosRandom : xPosStandard, rangeLineTransform.localPosition.y, 0);
+    }
+
+    void Start()
+    {
+        xPosRandom = rangeLineTransform.localPosition.x;
+        xPosStandard = xPosRandom - 185;
+        toggle.onValueChanged.AddListener(delegate { Switch(); });
+    }
+}
