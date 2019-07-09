ba8353c95ee8e3e03c99d162add87d4cf6e40979
FHomps <francois.homps@ecl17.ec-lyon.fr>
2019-07-09 17:15:52 +0200

    implemented standard struct for json loadingé

--- ReViVD Control Panel/Assets/Launcher.cs
+++ ReViVD Control Panel/Assets/Launcher.cs
@@ -12,6 +12,87 @@ public class Launcher : MonoBehaviour
     [SerializeField] GameObject spheres;
     [SerializeField] GameObject style;
 
+    public struct AssetBundle {
+        string name;
+        string filename;
+        Vector3 position;
+        Vector3 rotation;
+        Vector3 scale;
+    }
+
+    public enum DataType {
+        int32,
+        float32,
+        float64
+    }
+
+    public enum PathAttributeRole {
+        n_atoms,
+        id,
+        other
+    }
+
+    public struct PathAttribute {
+        string name;
+        DataType type;
+        PathAttributeRole role;
+        bool randomColor;
+        Color32 colorStart;
+        Color32 colorEnd;
+        float valueColorStart;
+        float valueColorEnd;
+    }
+
+    public enum AtomAttributeRole {
+        x,
+        y,
+        z,
+        t,
+        lat,
+        lon,
+        other
+    }
+
+    public struct AtomAttribute {
+        string name;
+        DataType type;
+        AtomAttributeRole role;
+        Color32 colorStart;
+        Color32 colorEnd;
+        float valueColorStart;
+        float valueColorEnd;
+    }
+
+    public struct LoadingData {
+        string filename;
+        bool endianness;
+        Vector3 sizeCoeff;
+        Vector3 districtSize;
+        Vector3 minPoint;
+        Vector3 maxPoint;
+        Vector2 gpsOrigin;
+
+        int file_n_paths;
+        bool randomPaths;
+        int chosen_n_paths;
+        int chosen_paths_start;
+        int chosen_paths_end;
+        int chosen_paths_step;
+
+        bool constant_n_instants;
+        int file_n_instants;
+        int chosen_instants_start;
+        int chosen_instants_end;
+        int chosen_instants_step;
+
+        float spheresRadius;
+        float spheresAnimSpeed;
+        float spheresGlobalTime;
+
+        AssetBundle[] assetBundles;
+        PathAttribute[] pathAttributes;
+        AtomAttribute[] atomAttributes;
+    };
 
     public void LoadJSon() {
 
